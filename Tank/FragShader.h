107d8ca4edb896d84573e00ce3e0159bdd883237
Grober Michael <58160994+leviathan-7@users.noreply.github.com>
2022-12-21 18:01:40 +0300

    Изменения в шейдере

--- Tank/FragShader.h
+++ Tank/FragShader.h
@@ -28,24 +28,14 @@ const char* FragShaderSource = R"(
         vec4 spec = textureColor * pow(max(dot(lightDir, reflectedViewDir), 0.0f), 8.0f);
         return (diff + spec) * intensivity;
     }
-    vec4 shadeMinnaert(vec3 light_direction, vec3 view_direction, float intensivity)
-    {
-        vec4 diffColor = texture(texture, vs_texcoord);
-        float k = 0.8f;
-        vec3 normal = normalize(vs_normal);
-        vec3 lightDir = normalize(light_direction);
-        vec3 viewDir = normalize(view_direction);
-        float d1 = pow(max(dot(normal, lightDir), 0.0f), 1.0f + k);
-        float d2 = pow(1.0f - dot(normal, viewDir), 1.0f - k);
-        return diffColor * d1 * d2 * intensivity;
-    }
+
     void main() {
 
-        vec3 dir_light_direction = vec3(10.0f,30.0f,0.0f);
+        vec3 dir_light_direction = vec3(10.0f,7.0f,25.0f);
         
-        float dir_light_intensivity = 0.7f;
+        float dir_light_intensivity = 0.5f;
         vec3 view_direction = view_position - vs_position;
-        color = texture(texture, vs_texcoord) + shadeMinnaert(dir_light_direction, view_direction, dir_light_intensivity);
+        color = texture(texture, vs_texcoord) + shadePhong(dir_light_direction, view_direction, dir_light_intensivity);
     }
 )";
 
